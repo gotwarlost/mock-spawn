514ecd295893a2529c0cada40b22282bd62b50c2
Leonid Beschastny <lbeschastny@iknow.travel>
2015-05-25 17:23:51 +0300

    Fixed race condition in node 0.8.x

--- index.js
+++ index.js
@@ -61,6 +61,7 @@ MockProcess.prototype._start = function (command, args, opts) {
     if(runner.throws instanceof Error){
         throw runner.throws;
     }
+    this.stdin.pause();
     process.nextTick(function () {
         runner.call(that, function (exitCode) {
             if(!that.ended){
@@ -71,6 +72,7 @@ MockProcess.prototype._start = function (command, args, opts) {
                 that.stderr.end();
             }
         });
+        that.stdin.resume();
     });
     return this;
 };
